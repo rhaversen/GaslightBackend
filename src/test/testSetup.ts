2333d2fd37f099de87a73df4a4ece86465498f28
Rasmus Haversen <rhaversen@gmail.com>
2024-11-04 04:50:49 +0100

    Add dev secrets

--- src/test/testSetup.ts
+++ src/test/testSetup.ts
@@ -22,6 +22,7 @@ import { disconnectFromInMemoryMongoDB } from './mongoMemoryReplSetConnector.js'
 // Test environment settings
 process.env.NODE_ENV = 'test'
 process.env.SESSION_SECRET = 'TEST_SESSION_SECRET'
+process.env.MICROSERVICE_AUTHORIZATION = 'TEST_MICROSERVICE_AUTHORIZATION'
 
 // Global variables
 const chaiHttpObject = chai.use(chaiHttp)
