09917ae927f45d7d10c6a960cd424127703c7bae
Rasmus Haversen <rhaversen@gmail.com>
2024-11-04 22:34:30 +0100

    add service route

---
+++ src/app/routes/service.ts
@@ -0,0 +1,51 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import { Router } from 'express'
+import mongoose from 'mongoose'
+
+// Own modules
+
+// Environment variables
+
+// Config variables
+
+// Destructuring and global variables
+const router = Router()
+
+/**
+ * @route GET /api/service/livez
+ * @description Check if the server is live.
+ * @access Public
+ * @returns {number} res.status - The status code of the HTTP response.
+ */
+router.get('/livez', (_req, res) => {
+	res.status(200).send('OK')
+})
+
+/**
+ * @route GET /api/service/readyz
+ * @description Check if the database and Socket.io are ready.
+ * @access Public
+ * @returns {number} res.status - The status code of the HTTP response.
+ */
+router.get('/readyz', (_req, res) => {
+	const mongooseReady = mongoose.connection.readyState === 1
+	if (mongooseReady) {
+		return res.status(200).send('OK')
+	} else {
+		return res.status(503).send('Database or Socket.io unavailable')
+	}
+})
+
+/**
+ * @route GET /api/service/debug-sentry
+ * @description Throw an error to test Sentry.
+ * @access Public
+ * @returns {number} res.status - The status code of the HTTP response.
+ */
+router.get('/debug-sentry', () => {
+	throw new Error('Sentry error')
+})
+
+export default router
