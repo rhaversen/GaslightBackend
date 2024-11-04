39055a7afefc0b093d01439898d014f34fb17e9b
Rasmus Haversen <rhaversen@gmail.com>
2024-11-04 22:34:19 +0100

    add passport config

---
+++ src/app/utils/passportConfig.ts
@@ -0,0 +1,63 @@
+// Node.js built-in modules
+
+// Third-party libraries
+import { Strategy as LocalStrategy } from 'passport-local'
+import { type PassportStatic } from 'passport'
+
+// Own modules
+import UserModel, { type IUser } from '../models/User.js'
+
+// Environment variables
+
+// Config variables
+
+// Destructuring and global variables
+
+const configurePassport = (passport: PassportStatic): void => {
+	// Local User Strategy
+	passport.use('user-local', new LocalStrategy({
+		usernameField: 'email',
+		passwordField: 'password'
+	}, (email, password, done) => {
+		(async () => {
+			try {
+				const user = await UserModel.findOne({ email }).exec()
+				if (user === null || user === undefined) {
+					done(null, false, { message: 'User with email ' + email + ' does not exist.' })
+					return
+				}
+
+				const isMatch = await user.comparePassword(password)
+				if (!isMatch) {
+					done(null, false, { message: 'Invalid password' })
+					return
+				}
+
+				done(null, user)
+			} catch (err) {
+				done(err)
+			}
+		})().catch(err => { done(err) })
+	}))
+
+	passport.serializeUser(function (user, done) {
+		const userId = (user as IUser).id
+		done(null, userId)
+	})
+
+	passport.deserializeUser(function (id, done) {
+		UserModel.findById(id).exec()
+			.then(user => {
+				if (user !== null && user !== undefined) {
+					done(null, user) // Call done with the user object
+				} else {
+					done(new Error('User not found'), false) // Error handling for admin not found
+				}
+			})
+			.catch(err => {
+				done(err, false) // Error handling
+			})
+	})
+}
+
+export default configurePassport
