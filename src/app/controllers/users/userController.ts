5673b829ff3e16b5fc678ba12a8961e532d7048e
Rasmus Haversen <rhaversen@gmail.com>
2024-11-05 02:03:10 +0100

    Add user controller and route

--- src/app/controllers/users/userController.ts
+++ src/app/controllers/users/userController.ts
@@ -1,6 +1,38 @@
 // Node.js built-in modules
+
 // Third-party libraries
+import { type NextFunction, type Request, type Response } from 'express'
+
 // Own modules
+import UserModel from '../../models/User.js'
+import { loginUserLocal } from './authController'
+
 // Environment variables
+
 // Config variables
+
 // Destructuring and global variables
+
+export async function register(req: Request, res: Response, next: NextFunction): Promise<void> {
+	const body: Record<string, unknown> = {
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+		confirmPassword: req.body.confirmPassword
+	}
+
+	const existingUser = await UserModel.findOne({ email: body.email }).exec()
+
+	if (existingUser !== null) {
+		// User doesn't exist, create a new user
+		await UserModel.create({
+			username: body.username,
+			email: body.email,
+			password: body.password,
+			confirmed: true // TODO: Implement confirmation logic later
+		})
+	}
+
+	loginUserLocal(req, res, next)
+	next()
+}
--- src/app/routes/users/users.ts
+++ src/app/routes/users/users.ts
@@ -1,6 +1,37 @@
 // Node.js built-in modules
+
 // Third-party libraries
+import Router from 'express'
+
 // Own modules
+import asyncErrorHandler from '../../utils/asyncErrorHandler.js'
+
+// Controller functions
+import {
+	register
+} from '../../controllers/users/userController.js'
+
 // Environment variables
+
 // Config variables
+
 // Destructuring and global variables
+const router = Router()
+
+/**
+ * @route POST /api/v1/user/user
+ * @description Register user and return session cookie.
+ * @access Public
+ * @param {string} req.body.username - The username of the user.
+ * @param {string} req.body.email - The email of the user.
+ * @param {string} req.body.password - The password of the user.
+ * @param {string} req.body.confirmPassword - The password confirmation of the user.
+ * @returns {number} res.status - The status code of the HTTP response.
+ * @returns {Object} res.body - The user object.
+ * @returns {string} res.headers['set-cookie'] - The session cookie.
+ */
+router.post('/user',
+	asyncErrorHandler(register)
+)
+
+export default router
