5dd87fd216c1c915463adc271369e6be4df88ef4
Rasmus Haversen <rhaversen@gmail.com>
2024-11-27 06:07:06 +0100

    Update implementation to refelct code runner route

--- src/app/services/CodeRunner.ts
+++ src/app/services/CodeRunner.ts
@@ -11,23 +11,21 @@ import AppConfig from '../utils/setupConfig.js'
 // Environment variables
 
 // Config variables
-const { 
+const {
 	codeRunnerHost
 } = AppConfig
 
 // Destructuring and global variables
 
-export async function submitCodeForTest (submission: ISubmission): Promise<boolean> {
+export async function submitCodeForTest(submission: ISubmission): Promise<boolean> {
 	try {
-		const { code, _id: submissionId } = submission
-    
-		await axios.post(`http://${codeRunnerHost}/api/v1/submissions`, {
-			submissionId,
-			code
+
+		await axios.post(`http://${codeRunnerHost}/api/v1/grade-submission`, {
+			submission
 		})
-    
-		logger.info('Code submitted for test', submission.id )
-    
+
+		logger.info('Code submitted for test', submission.id)
+
 		return true
 	} catch (error) {
 		if (error instanceof Error) {
@@ -35,7 +33,7 @@ export async function submitCodeForTest (submission: ISubmission): Promise<boole
 		} else {
 			logger.error('Error submitting code for test', { error: String(error) })
 		}
-    
+
 		return false
 	}
 }
