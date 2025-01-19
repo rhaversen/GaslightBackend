49a2dcec587caf651d20ed8d88cfc425c7263394
Rasmus Haversen <rhaversen@gmail.com>
2025-01-19 04:47:10 +0100

    Seed database with test user credentials for development purposes

--- src/development/seedDatabase.ts
+++ src/development/seedDatabase.ts
@@ -3,11 +3,14 @@
 // file deepcode ignore NoHardcodedCredentials/test: Hardcoded credentials are only used for testing purposes
 // file deepcode ignore HardcodedNonCryptoSecret/test: Hardcoded credentials are only used for testing purposes
 
+import UserModel from '../app/models/User.js'
 import logger from '../app/utils/logger.js'
 
 logger.info('Seeding database')
 
-
-// Products for every hour of the day
+await UserModel.create({
+	email: 'test@test.com',
+	password: 'password',
+})
 
 logger.info('Database seeded')
