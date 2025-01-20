fb09848462cca05add204ec4ebb6233a089c0826
Rasmus Haversen <rhaversen@gmail.com>
2025-01-20 21:35:48 +0100

    Seed database with initial user and multiple submission examples

--- src/development/seedDatabase.ts
+++ src/development/seedDatabase.ts
@@ -3,14 +3,105 @@
 // file deepcode ignore NoHardcodedCredentials/test: Hardcoded credentials are only used for testing purposes
 // file deepcode ignore HardcodedNonCryptoSecret/test: Hardcoded credentials are only used for testing purposes
 
+import SubmissionModel from '../app/models/Submission.js'
 import UserModel from '../app/models/User.js'
 import logger from '../app/utils/logger.js'
 
 logger.info('Seeding database')
 
-await UserModel.create({
+const user = await UserModel.create({
 	email: 'test@test.com',
 	password: 'password',
+})
+
+await SubmissionModel.create({
+	title: 'Hello, world!',
+	code: 'print("Hello, world!")',
+	user: user.id,
+})
+
+await SubmissionModel.create({
+	title: 'dumbStrategy',
+	code: `const main = (api: MeyerStrategyAPI) => {
+	// If we're first in the round, we need to roll
+	if (api.isFirstInRound()) {
+		api.roll()
+		return
+	}
+
+	// Randomly reveal
+	if (Math.random() > 0.5) {
+		api.reveal()
+		return
+	}
+
+	// Get previous announced value
+	const lastScore = api.getPreviousAction()
+
+	// Roll the dice
+	const currentScore = api.roll()
+
+	// If our score is higher or equal, finish the turn
+	if (lastScore === null || currentScore >= lastScore) {
+		return
+	}
+
+	// If our score is lower, we can either lie or call "det eller derover"
+	if (Math.random() > 0.5) {
+		api.lie(lastScore)
+	} else {
+		api.detEllerDerover()
+	}
+}
+
+export default main`,
+	user: user.id,
+})
+
+await SubmissionModel.create({
+	title: 'honestStrategy',
+	code: `const main = (api: MeyerStrategyAPI) => {
+	// If we're first in the round, we need to roll
+	if (api.isFirstInRound()) {
+		api.roll()
+		return
+	}
+
+	// Get previous announced value
+	const lastScore = api.getPreviousAction()
+
+	// Roll the dice
+	const currentScore = api.roll()
+
+	// If our score is higher or equal, finish the turn
+	if (lastScore === null || currentScore >= lastScore) {
+		return
+	}
+
+	// If our score is lower, we call "det eller derover"
+	api.detEllerDerover()
+}
+
+export default main`,
+	user: user.id,
+})
+
+await SubmissionModel.create({
+	title: 'lyingStrategy',
+	code: `const main = (api: MeyerStrategyAPI) => {
+	// If we're first in the round, we need to roll
+	if (api.isFirstInRound()) {
+		api.roll()
+		return
+	}
+
+	// We always lie with maximum score
+	api.roll()
+	api.lie(1000)
+}
+
+export default main`,
+	user: user.id,
 })
 
 logger.info('Database seeded')
